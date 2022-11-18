15fbcb8afd955b409bc58d07ea2ecbdf506ca0b0
Jan Sviland <jan.sviland@systek.no>
2022-11-18 15:31:06 +0100

    basic setup

---
+++ 2022/AdventOfCode.2022.Day1/Day1Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode._2022.Day1;
+
+public static class Day1Solution
+{
+    public static int Solve(int input)
+    {
+        return 420;
+    }
+}
---
+++ 2022/AdventOfCode.2022.Day1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventOfCode._2022.Day1;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("args: " + string.Join(", ", args));
+
+        var result = Day1Solution.Solve(123);
+        Console.WriteLine("Result: " + result);
+    }
+}
