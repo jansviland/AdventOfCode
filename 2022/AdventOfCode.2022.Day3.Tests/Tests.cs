0d62baa0e45db777d4d29a1f8a5521f7547b0a6c
Jan Sviland <jan.sviland@systek.no>
2022-12-03 14:19:04 +0100

    test case for day 3

--- 2022/AdventOfCode.2022.Day3.Tests/Tests.cs
+++ 2022/AdventOfCode.2022.Day3.Tests/Tests.cs
@@ -33,17 +33,46 @@ public class Tests : TestBed<TestFixture>
     }
 
     [Fact]
-    public void ParseStringPart1Test()
+    public void ParseStringPart1Test1()
     {
         // arrange
         // act
         // assert
-        Assert.Equal('p', _solutionService.ParseStringPart1(_input[0]));
-        Assert.Equal('L', _solutionService.ParseStringPart1(_input[1]));
-        Assert.Equal('P', _solutionService.ParseStringPart1(_input[2]));
-        Assert.Equal('v', _solutionService.ParseStringPart1(_input[3]));
-        Assert.Equal('t', _solutionService.ParseStringPart1(_input[4]));
-        Assert.Equal('s', _solutionService.ParseStringPart1(_input[5]));
+        var rucksack1 = _solutionService.ParseStringPart1(_input[0]);
+        Assert.Equal('p', rucksack1.WrongItem);
+        Assert.Equal(16, rucksack1.Priority);
+        Assert.Equal("vJrwpWtwJgWr", rucksack1.Compartment1);
+        Assert.Equal("hcsFMMfFFhFp", rucksack1.Compartment2);
+
+        var rucksack2 = _solutionService.ParseStringPart1(_input[1]);
+        Assert.Equal('L', rucksack2.WrongItem);
+        Assert.Equal(38, rucksack2.Priority);
+        Assert.Equal("jqHRNqRjqzjGDLGL", rucksack2.Compartment1);
+        Assert.Equal("rsFMfFZSrLrFZsSL", rucksack2.Compartment2);
+
+        var rucksack3 = _solutionService.ParseStringPart1(_input[2]);
+        Assert.Equal('P', rucksack3.WrongItem);
+        Assert.Equal(42, rucksack3.Priority);
+        Assert.Equal("PmmdzqPrV", rucksack3.Compartment1);
+        Assert.Equal("vPwwTWBwg", rucksack3.Compartment2);
+
+        var rucksack4 = _solutionService.ParseStringPart1(_input[3]);
+        Assert.Equal('v', rucksack4.WrongItem);
+        Assert.Equal(22, rucksack4.Priority);
+        Assert.Equal("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", rucksack4.Compartment1);
+        Assert.Equal("", rucksack4.Compartment2);
+
+        var rucksack5 = _solutionService.ParseStringPart1(_input[4]);
+        Assert.Equal('t', rucksack5.WrongItem);
+        Assert.Equal(20, rucksack5.Priority);
+        Assert.Equal("ttgJtRGJQctTZtZT", rucksack5.Compartment1);
+        Assert.Equal("", rucksack5.Compartment2);
+
+        var rucksack6 = _solutionService.ParseStringPart1(_input[5]);
+        Assert.Equal('s', rucksack6.WrongItem);
+        Assert.Equal(19, rucksack6.Priority);
+        Assert.Equal("CrZsJsPPZsGzwwsLwLmpwMDw", rucksack6.Compartment1);
+        Assert.Equal("", rucksack6.Compartment2);
     }
 
     [Fact]
--- 2022/AdventOfCode.2022.Day3/ISolutionService.cs
+++ 2022/AdventOfCode.2022.Day3/ISolutionService.cs
@@ -3,8 +3,16 @@ namespace AdventOfCode._2022.Day3;
 public interface ISolutionService
 {
     public int RunPart1(string[] input);
-    public char ParseStringPart1(string input);
+    public Rucksack ParseStringPart1(string input);
     public int RunPart2(string[] input);
+}
+
+public class Rucksack
+{
+    public int Priority { get; set; }
+    public char WrongItem { get; set; }
+    public string Compartment1 { get; set; }
+    public string Compartment2 { get; set; }
 }
 
 public class SolutionService : ISolutionService
@@ -24,7 +32,7 @@ public class SolutionService : ISolutionService
         throw new NotImplementedException();
     }
 
-    public char ParseStringPart1(string input)
+    public Rucksack ParseStringPart1(string input)
     {
         _logger.LogInformation("Parsing {Input}", input);
 
