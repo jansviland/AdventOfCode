5d5401ca199460f293a0f3d32e9f7f0bdd443308
Jan Sviland <jan.sviland@systek.no>
2022-12-14 08:10:16 +0100

    add empty project 11 to 14

---
+++ 2022/AdventOfCode.2022.Day13/ISolutionService.cs
@@ -0,0 +1,33 @@
+namespace AdventOfCode._2022.Day13;
+
+public interface ISolutionService
+{
+    public int RunPart1(string[] input);
+    public int RunPart2(string[] input);
+}
+
+public class SolutionService : ISolutionService
+{
+    private readonly ILogger<SolutionService> _logger;
+
+    public SolutionService(ILogger<SolutionService> logger)
+    {
+        _logger = logger;
+    }
+
+    public int RunPart1(string[] input)
+    {
+        _logger.LogInformation("Solving day 13");
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+    }
+
+    public int RunPart2(string[] input)
+    {
+        _logger.LogInformation("Solving day 13 - Part 2");
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+    }
+}
---
+++ 2022/AdventOfCode.2022.Day14/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Diagnostics;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace AdventOfCode._2022.Day14;
+
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        var stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        var builder = new ConfigurationBuilder();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(BuildConfiguration(builder))
+            .Enrich.FromLogContext()
+            .CreateLogger();
+
+        var host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(((_, collection) => { collection.AddTransient<ISolutionService, SolutionService>(); }))
+            .UseSerilog()
+            .Build();
+
+        Log.Logger.Information("args: {AllArguments}", string.Join(", ", args));
+
+        var svc = ActivatorUtilities.CreateInstance<SolutionService>(host.Services);
+
+        string[] input;
+        if (args.Length == 0)
+        {
+            input = File.ReadAllLines("Assets/input.txt");
+        }
+        else
+        {
+            input = File.ReadAllLines(args[0]);
+        }
+
+        var result = svc.RunPart1(input);
+        Log.Logger.Information("result: {Result}", result);
+
+        var resultPart2 = svc.RunPart2(input);
+        Log.Logger.Information("result: {Result}", resultPart2);
+
+        stopWatch.Stop();
+        Log.Logger.Information("Elapsed time: {Elapsed} ms", stopWatch.ElapsedMilliseconds);
+    }
+
+    private static IConfiguration BuildConfiguration(IConfigurationBuilder builder)
+    {
+        builder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .AddEnvironmentVariables();
+
+        var configuration = builder.Build();
+
+        return configuration;
+    }
+}
