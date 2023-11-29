1aa7b0a4ab13b9b2add1e3f2a0422c7b33fd3dcb
Jan Sviland <jan.sviland@systek.no>
2023-11-29 10:40:48 +0100

    prepare project 2023 day 1

---
+++ 2023/AdventOfCode.2023.Day1/ISolutionService.cs
@@ -0,0 +1,34 @@
+namespace AdventOfCode._2023.Day1;
+
+public interface ISolutionService
+{
+    public int RunPart1(string[] input);
+    public int RunPart2(string[] input);
+}
+
+public class SolutionService : ISolutionService
+{
+    private readonly ILogger<SolutionService> _logger;
+
+    public SolutionService(ILogger<SolutionService> logger)
+    {
+        _logger = logger;
+    }
+
+    public int RunPart1(string[] input)
+    {
+        _logger.LogInformation("Solving - 2023 - Day 1 - Part 1");
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+
+    }
+
+    public int RunPart2(string[] input)
+    {
+        _logger.LogInformation("Solving - 2023 - day 1 - Part 2");
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+    }
+}
