88f9f5fee3475d4809785038f327ca626a013f7a
Jan Sviland <jan.sviland@systek.no>
2023-12-08 09:46:15 +0100

    solved part 1

--- 2023/AdventOfCode.2023.Day8.Tests/Tests.cs
+++ 2023/AdventOfCode.2023.Day8.Tests/Tests.cs
@@ -5,11 +5,14 @@ public class Tests : TestBed<TestFixture>
     private readonly ISolutionService _solutionService;
 
     private readonly string[] _input;
+    private readonly string[] _inputPart2;
 
     public Tests(ITestOutputHelper testOutputHelper, TestFixture fixture) : base(testOutputHelper, fixture)
     {
         _solutionService = _fixture.GetService<ISolutionService>(_testOutputHelper)!;
+
         _input = File.ReadAllLines("Assets/test-input.txt");
+        _inputPart2 = File.ReadAllLines("Assets/test-input-part2.txt");
     }
 
     [Fact]
@@ -22,7 +25,7 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart1(_input);
 
         // assert
-        Assert.Equal(6440, result);
+        Assert.Equal(2, result);
     }
 
     [Fact]
@@ -32,9 +35,9 @@ public class Tests : TestBed<TestFixture>
         _testOutputHelper.WriteLine("Running unit test - 2023 - Day 8 - Part 2");
 
         // act
-        var result = _solutionService.RunPart2(_input);
+        var result = _solutionService.RunPart2(_inputPart2);
 
         // assert
-        Assert.Equal(5905, result);
+        Assert.Equal(6, result);
     }
 }
--- 2023/AdventOfCode.2023.Day8/ISolutionService.cs
+++ 2023/AdventOfCode.2023.Day8/ISolutionService.cs
@@ -6,6 +6,18 @@ public interface ISolutionService
     public int RunPart2(string[] input);
 }
 
+public class Node
+{
+    // public string Value { get; set; }
+
+    public string Value { get; set; } = null!;
+    public string Left { get; set; } = null!;
+    public string Right { get; set; } = null!;
+
+    // public Tree Left { get; set; }
+    // public Tree Right { get; set; }
+}
+
 public class SolutionService : ISolutionService
 {
     private readonly ILogger<ISolutionService> _logger;
@@ -20,7 +32,83 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - 2023 - Day 8 - Part 1");
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        throw new NotImplementedException();
+        var nodes = new Dictionary<string, Node>();
+        for (var i = 2; i < input.Length; i++)
+        {
+            string key = input[i].Split('=')[0].Trim();
+            string[] leftAndRight = input[i].Split('=')[1].Trim().Split(',');
+            string left = leftAndRight[0].Replace('(', ' ').Replace(')', ' ').Replace(',', ' ').Trim();
+            string right = leftAndRight[1].Replace('(', ' ').Replace(')', ' ').Replace(',', ' ').Trim();
+
+            if (nodes.TryGetValue(key, out var newNode))
+            {
+            }
+            else
+            {
+                // create new node
+                nodes.Add(key, new Node
+                {
+                    Value = key,
+                    Left = left,
+                    Right = right
+                });
+            }
+        }
+
+        var instructions = input[0];
+
+        var goal = "ZZZ";
+        var start = "AAA";
+        nodes.TryGetValue(start, out var current);
+
+        if (current == null)
+        {
+            throw new Exception("Could not find start node");
+        }
+
+        var totalSteps = 0;
+        var step = 0;
+        while (current.Value != goal)
+        {
+            // start from the beginning when we reach the end of the instructions
+            if (step >= instructions.Length)
+            {
+                step = 0;
+            }
+
+            var instruction = instructions[step];
+            if (instruction == 'L')
+            {
+                // go left
+                nodes.TryGetValue(current.Left, out var leftNode);
+                if (leftNode == null)
+                {
+                    throw new Exception("Could not find left node");
+                }
+
+                current = leftNode;
+            }
+            else if (instruction == 'R')
+            {
+                // go right
+                nodes.TryGetValue(current.Right, out var rightNode);
+                if (rightNode == null)
+                {
+                    throw new Exception("Could not find right node");
+                }
+
+                current = rightNode;
+            }
+            else
+            {
+                throw new Exception("Unknown instruction");
+            }
+
+            step++;
+            totalSteps++;
+        }
+
+        return totalSteps;
     }
 
     public int RunPart2(string[] input)
