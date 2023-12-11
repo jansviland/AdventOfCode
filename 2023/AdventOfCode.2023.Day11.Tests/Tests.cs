31fcc68daf6ed0575811133c044a8c14c157923b
Jan Sviland <jan.sviland@systek.no>
2023-12-11 22:15:54 +0100

    WIP day 11

--- 2023/AdventOfCode.2023.Day11.Tests/Tests.cs
+++ 2023/AdventOfCode.2023.Day11.Tests/Tests.cs
@@ -5,13 +5,29 @@ public class Tests : TestBed<TestFixture>
     private readonly ISolutionService _solutionService;
 
     private readonly string[] _input;
+    private readonly string[] _input2;
 
     public Tests(ITestOutputHelper testOutputHelper, TestFixture fixture) : base(testOutputHelper, fixture)
     {
         _solutionService = _fixture.GetService<ISolutionService>(_testOutputHelper)!;
         _input = File.ReadAllLines("Assets/test-input.txt");
+        _input2 = File.ReadAllLines("Assets/test-input2.txt");
     }
 
+    
+    [Fact]
+    public void Part1ExpandGridTest()
+    {
+        // arrange
+        _testOutputHelper.WriteLine("Running unit test - 2023 - Day 11 - Part 1");
+
+        // act
+        string[] result = _solutionService.ExpandGrid(_input);
+
+        // assert
+        Assert.Equivalent(_input2, result);
+    }
+    
     [Fact]
     public void Part1Test()
     {
--- 2023/AdventOfCode.2023.Day11/ISolutionService.cs
+++ 2023/AdventOfCode.2023.Day11/ISolutionService.cs
@@ -4,6 +4,7 @@ public interface ISolutionService
 {
     public long RunPart1(string[] input);
     public long RunPart2(string[] input);
+    public string[] ExpandGrid(string[] input);
 }
 
 public class SolutionService : ISolutionService
@@ -20,6 +21,12 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - 2023 - Day 11 - Part 1");
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
+        var expanded = ExpandGrid(input);
+        var grid = Parse(expanded);
+        
+        // find distances
+        var distances = new Dictionary<(int, int), int>();
+        
         throw new NotImplementedException();
     }
 
@@ -29,5 +36,67 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
         throw new NotImplementedException();
+    }
+
+    public char[,] Parse(string[] input)
+    {
+        var result = new char[input.Length, input[0].Length];
+        for (var y = 0; y < input.Length; y++)
+        {
+            for (var x = 0; x < input[0].Length; x++)
+            {
+                result[y, x] = input[y][x];
+            }
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// Transpose a 2D array
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public string[] Transpose(string[] input)
+    {
+        var result = new string[input[0].Length];
+        for (var y = 0; y < input.Length; y++)
+        {
+            for (var x = 0; x < input[0].Length; x++)
+            {
+                result[x] += input[y][x];
+            }
+        }
+
+        return result;
+    }
+
+    public string[] ExpandGrid(string[] input)
+    {
+        var expandedRows = Expand(input);
+        var columns = Transpose(expandedRows);
+        var expandedColumns = Expand(columns);
+        var backAgain = Transpose(expandedColumns);
+
+        return backAgain;
+    }
+
+    public string[] Expand(string[] input)
+    {
+        var newRows = new List<string>();
+        foreach (string line in input)
+        {
+            if (line.ToArray().Any(x => x == '#'))
+            {
+                newRows.Add(line);
+            }
+            else
+            {
+                newRows.Add(line);
+                newRows.Add(line);
+            }
+        }
+
+        return newRows.ToArray();
     }
 }
