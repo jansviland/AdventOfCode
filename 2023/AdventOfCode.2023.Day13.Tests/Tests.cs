cc18b5737ea8946a502f6fbefb6104fe94a25743
Jan Sviland <jan.sviland@systek.no>
2023-12-12 08:49:07 +0100

    prepare day 13

---
+++ 2023/AdventOfCode.2023.Day13.Tests/Tests.cs
@@ -0,0 +1,40 @@
+namespace AdventOfCode._2023.Day13.Tests;
+
+public class Tests : TestBed<TestFixture>
+{
+    private readonly ISolutionService _solutionService;
+
+    private readonly string[] _input;
+
+    public Tests(ITestOutputHelper testOutputHelper, TestFixture fixture) : base(testOutputHelper, fixture)
+    {
+        _solutionService = _fixture.GetService<ISolutionService>(_testOutputHelper)!;
+        _input = File.ReadAllLines("Assets/test-input.txt");
+    }
+
+    [Fact]
+    public void Part1Test()
+    {
+        // arrange
+        _testOutputHelper.WriteLine("Running unit test - 2023 - Day 13 - Part 1");
+
+        // act
+        var result = _solutionService.RunPart1(_input);
+
+        // assert
+        Assert.Equal(114, result);
+    }
+
+    [Fact]
+    public void Part2Test()
+    {
+        // arrange
+        _testOutputHelper.WriteLine("Running unit test - 2023 - Day 13 - Part 2");
+
+        // act
+        var result = _solutionService.RunPart2(_input);
+
+        // assert
+        Assert.Equal(5905, result);
+    }
+}
