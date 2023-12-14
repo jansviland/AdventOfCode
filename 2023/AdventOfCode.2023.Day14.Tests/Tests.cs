4ddd0937a23ba4d188949a2ea4dbaf2e21bd9a06
Jan Sviland <jan.sviland@systek.no>
2023-12-14 15:38:59 +0100

    day 14 input

--- 2023/AdventOfCode.2023.Day14.Tests/Tests.cs
+++ 2023/AdventOfCode.2023.Day14.Tests/Tests.cs
@@ -22,7 +22,7 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart1(_input);
 
         // assert
-        Assert.Equal(114, result);
+        Assert.Equal(136, result);
     }
 
     [Fact]
