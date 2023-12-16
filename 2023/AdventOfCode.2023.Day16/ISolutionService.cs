3cdd748a6b80e9cf3f3bb68c90510462cf48be5c
Jan Sviland <jan.sviland@systek.no>
2023-12-16 14:08:33 +0100

    WIP day 16

--- 2023/AdventOfCode.2023.Day16/ISolutionService.cs
+++ 2023/AdventOfCode.2023.Day16/ISolutionService.cs
@@ -1,3 +1,5 @@
+using Common;
+
 namespace AdventOfCode._2023.Day16;
 
 public interface ISolutionService
@@ -15,10 +17,217 @@ public class SolutionService : ISolutionService
         _logger = logger;
     }
 
+    public void PrintGrid(Cell[,] grid)
+    {
+        var numRows = grid.GetLength(0);
+        var numCols = grid.GetLength(1);
+
+        var sb = new StringBuilder();
+        sb.AppendLine();
+
+        for (var row = 0; row < numRows; row++)
+        {
+            for (var col = 0; col < numCols; col++)
+            {
+                var value = "";
+
+                if (grid[row, col].Steps != -1)
+                {
+                    value = grid[row, col].Steps.ToString();
+                    // sb.Append(grid[row, col].Steps);
+                }
+                else
+                {
+                    value = grid[row, col].Value.ToString();
+                    // sb.Append(grid[row, col].Value);
+                }
+
+                sb.Append(value.PadLeft(3, ' '));
+            }
+
+            sb.AppendLine();
+        }
+
+        _logger.LogInformation(sb.ToString());
+    }
+
     public long RunPart1(string[] input)
     {
         _logger.LogInformation("Solving - 2023 - Day 16 - Part 1");
         _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        var grid = Grid.CreateGrid(input);
+        PrintGrid(grid);
+
+        var paddedGrid = grid.ApplyPadding();
+
+        // top left corner
+        var start = paddedGrid[1, 1];
+        // start.Value = '-';
+        start.Steps = 0;
+        start.Direction = Direction.Right;
+
+        PrintGrid(paddedGrid);
+
+        var possibleValues = new Queue<Cell>();
+        possibleValues.Enqueue(start);
+
+        var visited = new HashSet<Cell>();
+
+        while (possibleValues.Any())
+        {
+            var current = possibleValues.Dequeue();
+            visited.Add(current);
+
+            var newPaths = new List<Cell>();
+
+            if (current.Value == 'X')
+            {
+                continue;
+            }
+
+            var up = paddedGrid[current.X, current.Y - 1];
+            up.Direction = Direction.Up;
+            var down = paddedGrid[current.X, current.Y + 1];
+            down.Direction = Direction.Down;
+            var right = paddedGrid[current.X + 1, current.Y];
+            right.Direction = Direction.Right;
+            var left = paddedGrid[current.X - 1, current.Y];
+            left.Direction = Direction.Left;
+
+            switch (current.Value)
+            {
+                case '.':
+                    // If the beam encounters empty space (.), it continues in the same direction.
+                    switch (current.Direction)
+                    {
+                        case Direction.Up:
+                            newPaths.Add(up);
+                            break;
+                        case Direction.Down:
+                            newPaths.Add(down);
+                            break;
+                        case Direction.Left:
+                            newPaths.Add(left);
+                            break;
+                        case Direction.Right:
+                            newPaths.Add(right);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+
+                // If the beam encounters a mirror (/ or \), the beam is reflected 90 degrees depending on the angle of the mirror.
+                // For instance, a rightward-moving beam that encounters a / mirror would continue upward in the mirror's column, while a rightward-moving beam that encounters a \ mirror
+                // would continue downward from the mirror's column.
+                case '/':
+                    switch (current.Direction)
+                    {
+                        case Direction.Up:
+                            newPaths.Add(right);
+                            break;
+                        case Direction.Down:
+                            newPaths.Add(left);
+                            break;
+                        case Direction.Left:
+                            newPaths.Add(down);
+                            break;
+                        case Direction.Right:
+                            newPaths.Add(up);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+
+                case '\'':
+                    switch (current.Direction)
+                    {
+                        case Direction.Up:
+                            newPaths.Add(left);
+                            break;
+                        case Direction.Down:
+                            newPaths.Add(right);
+                            break;
+                        case Direction.Left:
+                            newPaths.Add(up);
+                            break;
+                        case Direction.Right:
+                            newPaths.Add(down);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+
+                    }
+                    break;
+
+                // If the beam encounters the pointy end of a splitter (| or -), the beam passes through the splitter as if the splitter were empty space.
+                // For instance, a rightward-moving beam that encounters a - splitter would continue in the same direction.
+
+                case '|':
+                    switch (current.Direction)
+                    {
+                        case Direction.Up:
+                            newPaths.Add(up);
+                            break;
+                        case Direction.Down:
+                            newPaths.Add(down);
+                            break;
+                        case Direction.Left:
+                            newPaths.Add(up);
+                            newPaths.Add(down);
+                            break;
+                        case Direction.Right:
+                            newPaths.Add(up);
+                            newPaths.Add(down);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+
+                case '-':
+                    switch (current.Direction)
+                    {
+                        case Direction.Up:
+                            newPaths.Add(up);
+                            newPaths.Add(down);
+                            break;
+                        case Direction.Down:
+                            newPaths.Add(down);
+                            newPaths.Add(up);
+                            break;
+                        case Direction.Left:
+                            newPaths.Add(left);
+                            break;
+                        case Direction.Right:
+                            newPaths.Add(right);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                    break;
+
+            }
+
+            foreach (var cell in newPaths)
+            {
+                if (visited.Contains(cell))
+                {
+                    continue;
+                }
+
+                cell.Previous = current;
+                cell.Steps = current.Steps + 1;
+                // cell.Direction = current.Direction;
+
+                possibleValues.Enqueue(cell);
+            }
+
+            PrintGrid(paddedGrid);
+        }
+
 
         throw new NotImplementedException();
     }
--- Common/Grid/Grid.cs
+++ Common/Grid/Grid.cs
@@ -1,22 +1,102 @@
 ﻿namespace Common;
 
+public enum Direction
+{
+    Unknown,
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+public class Cell
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public char Value { get; set; }
+    public long Steps { get; set; } = -1;
+    public Cell? Previous { get; set; }
+    public Direction Direction { get; set; } = Direction.Unknown;
+
+    public override string ToString()
+    {
+        return $"{X},{Y} - {Value}";
+    }
+}
+
 public static class Grid
 {
-    // create a 2D array of chars
-    public static char[,] CreateGrid(string[] input)
+    public static Cell[,] ApplyPadding(this Cell[,] grid, char paddingValue = 'X')
     {
-        var grid = new char[input.Length, input[0].Length];
+        var numRows = grid.GetLength(0);
+        var numCols = grid.GetLength(1);
+
+        var newGrid = new Cell[numRows + 2, numCols + 2];
+
+        for (var row = 0; row < numRows + 2; row++)
+        {
+            for (var col = 0; col < numCols + 2; col++)
+            {
+                if (row == 0 || row == numRows + 1 || col == 0 || col == numCols + 1)
+                {
+                    newGrid[row, col] = new Cell
+                    {
+                        X = row,
+                        Y = col,
+                        Value = paddingValue
+                    };
+                }
+                else
+                {
+                    // newGrid[row, col] = grid[row - 1, col - 1];
+                    newGrid[row, col] = new Cell
+                    {
+                        X = row,
+                        Y = col,
+                        Value = grid[row - 1, col - 1].Value
+                    };
+                }
+            }
+        }
+
+        return newGrid;
+    }
+
+    // create a 2D array
+    public static Cell[,] CreateGrid(string[] input)
+    {
+        var grid = new Cell[input.Length, input[0].Length];
 
         for (var row = 0; row < input.Length; row++)
         {
             string line = input[row];
             for (var col = 0; col < line.Length; col++)
             {
-                grid[row, col] = line[col];
+                grid[row, col] = new Cell
+                {
+                    X = row,
+                    Y = col,
+                    Value = line[col]
+                };
             }
         }
 
         return grid;
+    }
+
+    public static List<T> GetRightValue<T>(this T[,] grid, int row, int col)
+    {
+        var rightValues = new List<T>();
+
+        int numRows = grid.GetLength(0);
+        int numCols = grid.GetLength(1);
+
+        if (numCols - 1 > col)
+        {
+            rightValues.Add(grid[row, col + 1]);
+        }
+
+        return rightValues;
     }
 
     public static List<T> GetAdjacentValues<T>(this T[,] grid, int row, int col)
@@ -26,8 +106,37 @@ public static class Grid
         int numRows = grid.GetLength(0);
         int numCols = grid.GetLength(1);
 
+        // Define the relative offsets for adjacent cells (4 possible directions).
+        int[] dx = { -1, 1, 0, 0 }; // left, right, up, down
+        int[] dy = { 0, 0, -1, 1 };
+
+        // Check each adjacent cell.
+        for (var i = 0; i < dx.Length; i++)
+        {
+            int newRow = row + dx[i];
+            int newCol = col + dy[i];
+
+            // Check if the adjacent cell is within the grid boundaries.
+            if (newRow >= 0 && newRow < numRows && newCol >= 0 && newCol < numCols)
+            {
+                // adjacentValues[i] = grid[newRow, newCol];
+                adjacentValues.Add(grid[newRow, newCol]);
+            }
+        }
+
+        return adjacentValues;
+    }
+
+    // includes diagonals
+    public static List<T> GetAllAdjacentValues<T>(this T[,] grid, int row, int col)
+    {
+        var adjacentValues = new List<T>();
+
+        int numRows = grid.GetLength(0);
+        int numCols = grid.GetLength(1);
+
         // Define the relative offsets for adjacent cells (8 possible directions).
-        int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 };
+        int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1 }; // left and up, left, left and down, up, down, right and up, right, right and down
         int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1 };
 
         // Check each adjacent cell.
