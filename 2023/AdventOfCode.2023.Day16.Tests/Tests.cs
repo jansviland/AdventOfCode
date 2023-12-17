40f36cde55b7294a5c1333fcf63c7625b6c09227
Jan Sviland <jan.sviland@systek.no>
2023-12-17 22:14:44 +0100

    solved part 2

--- 2023/AdventOfCode.2023.Day16.Tests/Tests.cs
+++ 2023/AdventOfCode.2023.Day16.Tests/Tests.cs
@@ -35,6 +35,6 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart2(_input);
 
         // assert
-        Assert.Equal(5905, result);
+        Assert.Equal(51, result);
     }
 }
--- 2023/AdventOfCode.2023.Day16/SolutionServiceV2.cs
+++ 2023/AdventOfCode.2023.Day16/SolutionServiceV2.cs
@@ -1,4 +1,12 @@
-﻿namespace AdventOfCode._2023.Day16;
+﻿using Common;
+
+namespace AdventOfCode._2023.Day16;
+
+public class Tile
+{
+    public char Value { get; set; }
+    public int Steps { get; set; }
+}
 
 public class SolutionServiceV2 : ISolutionService
 {
@@ -7,51 +15,148 @@ public class SolutionServiceV2 : ISolutionService
     private static readonly Complex Left = -Complex.One; // new Complex(-1.0, 0.0);
     private static readonly Complex Right = Complex.One; // new Complex(1.0, 0.0);
 
-    // create a dictionary to store the grid
-    private Dictionary<Complex, char> grid = new();
+    private static StringBuilder _sb = new StringBuilder();
 
-    // define a beam as a position and a direction
-    private (Complex position, Complex direction) beam;
+    private readonly ILogger<ISolutionService> _logger;
 
-    public SolutionServiceV2()
+    public SolutionServiceV2(ILogger<SolutionServiceV2> logger)
     {
-
+        _logger = logger;
     }
 
     public long RunPart1(string[] input, bool animated = false)
     {
-        var map = ParseInput(input);
+        var grid = ParseInput(input);
+        var count = EnergizedCells(grid, (Complex.Zero, Right));
 
-        var count = EnergizedCells(map, (Complex.Zero, Right));
+        PrintGrid(grid);
 
         return count;
     }
 
-    int EnergizedCells(Dictionary<Complex, char> map, (Complex position, Complex direction) beam)
+    public long RunPart2(string[] input)
     {
+        var grid = ParseInput(input);
+        // grid.ToImmutableDictionary();
 
+
+        // var max = 0;
+        // var beams = GetStartBeams(grid);
+        // foreach (var beam in beams)
+        // {
+        //     var gridCopy = grid.ToDictionary(x => x.Key, x => x.Value);
+        //     var result = EnergizedCells(gridCopy, beam);
+        //     _logger.LogInformation($"Starting beam at {beam.position} with direction {beam.direction} resulted in {result} cells being energized");
+        //     max = Math.Max(max, result);
+        //
+        //     // PrintGrid(gridCopy, true);
+        // }
+        //
+        // return max;
+
+        return (from beam in GetStartBeams(grid) select EnergizedCells(grid, beam)).Max();
+    }
+
+    private IEnumerable<(Complex position, Complex direction)> GetStartBeams(Dictionary<Complex, Tile> grid)
+    {
+        // get the bottom right corner
+        var br = grid.Keys.MaxBy(pos => pos.Imaginary + pos.Real);
+        return new[]
+        {
+            // when x == max x, go left
+            from pos in grid.Keys where pos.Real == br.Real select (pos, Left),
+
+            // when x == 0, go right
+            from pos in grid.Keys where pos.Real == 0 select (pos, Right),
+
+            // when y == max y, go up
+            from pos in grid.Keys where pos.Imaginary == br.Imaginary select (pos, Up),
+
+            // when y == 0, go down
+            from pos in grid.Keys where pos.Imaginary == 0 select (pos, Down),
+
+        }.SelectMany(x => x);
+    }
+
+    private void PrintGrid(Dictionary<Complex, Tile> grid, bool animated = false)
+    {
+        if (animated)
+        {
+            Console.Clear();
+            Console.SetCursorPosition(0, 0);
+        }
+
+        var minX = grid.Keys.Min(c => c.Real);
+        var maxX = grid.Keys.Max(c => c.Real);
+        var minY = grid.Keys.Min(c => c.Imaginary);
+        var maxY = grid.Keys.Max(c => c.Imaginary);
+
+        for (int y = (int)minY; y <= (int)maxY; y++)
+        {
+            for (int x = (int)minX; x <= (int)maxX; x++)
+            {
+                var pos = new Complex(x, y);
+                if (grid.TryGetValue(pos, out var tile))
+                {
+                    if (tile.Steps > 0)
+                    {
+                        _sb.Append(tile.Steps.ToString().PadLeft(3, ' '));
+                    }
+                    else
+                    {
+                        _sb.Append(tile.Value.ToString().PadLeft(3, ' '));
+                    }
+                }
+            }
+
+            _sb.AppendLine();
+        }
+
+        Console.WriteLine(_sb.ToString());
+        _sb.Clear();
+
+        if (animated)
+        {
+            Thread.Sleep(10);
+        }
+    }
+
+    private int EnergizedCells(Dictionary<Complex, Tile> grid, (Complex position, Complex direction) beam)
+    {
         // this is essentially just a flood fill algorithm.
         var q = new Queue<(Complex position, Complex direction)>();
         q.Enqueue(beam);
 
         var seen = new HashSet<(Complex position, Complex direction)>();
 
+        // var totalSteps = 0;
+
         while (q.TryDequeue(out beam))
         {
             seen.Add(beam);
-            foreach (var dir in Exits(map[beam.position], beam.direction))
+
+            // increment the steps
+            grid[beam.position].Steps++;
+
+            foreach (var dir in Exits(grid[beam.position], beam.direction))
             {
                 var pos = beam.position + dir;
-                if (map.ContainsKey(pos) && !seen.Contains((pos, dir)))
+                if (grid.ContainsKey(pos) && !seen.Contains((pos, dir)))
                 {
                     q.Enqueue((pos, dir));
                 }
             }
+
+            // if (totalSteps % 100 == 0)
+            // {
+            //     PrintGrid(grid, true);
+            // }
+            //
+            // totalSteps++;
         }
 
         return seen.Select(beam => beam.position).Distinct().Count();
     }
-
 
     /// <summary>
     /// instead of using a 2d array (two dimensional array), we use a dictionary with the key as a complex number
@@ -63,7 +168,7 @@ public class SolutionServiceV2 : ISolutionService
     /// </summary>
     /// <param name="input"></param>
     /// <returns></returns>
-    public Dictionary<Complex, char> ParseInput(string[] input)
+    public Dictionary<Complex, Tile> ParseInput(string[] input)
     {
         // var dictionary = new Dictionary<Complex, char>();
         // for (int irow = 0; irow < input.Length; irow++)
@@ -81,14 +186,19 @@ public class SolutionServiceV2 : ISolutionService
             from icol in Enumerable.Range(0, input[0].Length)
             let cell = input[irow][icol]
             let pos = new Complex(icol, irow)
-            select new KeyValuePair<Complex, char>(pos, cell)
+            let tile = new Tile
+            {
+                Value = cell,
+                Steps = 0
+            }
+            select new KeyValuePair<Complex, Tile>(pos, tile)
         ).ToDictionary();
     }
 
     // given a cell and a direction, return the possible exits (new directions from that cell)
-    public Complex[] Exits(char cell, Complex dir)
+    public Complex[] Exits(Tile tile, Complex dir)
     {
-        switch (cell)
+        switch (tile.Value)
         {
             case '-' when dir == Up || dir == Down:
                 return new[] { Left, Right };
@@ -105,8 +215,4 @@ public class SolutionServiceV2 : ISolutionService
         }
     }
 
-    public long RunPart2(string[] input)
-    {
-        throw new NotImplementedException();
-    }
 }
