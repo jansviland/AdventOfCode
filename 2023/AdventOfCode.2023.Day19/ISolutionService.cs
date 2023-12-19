543e033ccef4da14fb7597337b20a04367248185
Jan Sviland <jan.sviland@systek.no>
2023-12-19 16:13:44 +0100

    WIP

--- 2023/AdventOfCode.2023.Day19/ISolutionService.cs
+++ 2023/AdventOfCode.2023.Day19/ISolutionService.cs
@@ -9,6 +9,7 @@ public interface ISolutionService
 public class SolutionService : ISolutionService
 {
     private readonly ILogger<ISolutionService> _logger;
+    private readonly Dictionary<string, string> _workflows = new();
 
     public SolutionService(ILogger<SolutionService> logger)
     {
@@ -20,7 +21,55 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - 2023 - Day 19 - Part 1");
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        throw new NotImplementedException();
+        var endOfWorkflows = 0;
+        for (var i = 0; i < input.Length; i++)
+        {
+            string line = input[i];
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                endOfWorkflows = i + 1;
+                break;
+            }
+
+            var split = line.Split("{");
+            _workflows.Add(split[0], split[1].Substring(0, split[1].Length - 1));
+        }
+
+        long total = 0; 
+        for (var i = endOfWorkflows; i < input.Length; i++)
+        {
+            string line = input[i];
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                continue;
+            }
+            
+            var sum = sumOfApproved("in", line);
+            total += sum;
+        }
+
+        return total;
+    }
+
+    private long sumOfApproved(string workflowKey, string values)
+    {
+        var workflow = _workflows[workflowKey];
+        var workFlowSteps = workflow.Split(",");
+        
+        // foreach (var workFlowStep in workFlowSteps)
+        // R - Rejected. Return 0
+        // A - Approved. Return sum of values
+        // a string of letters, like lnx, pv or qqz, go to new workflow with that as key
+        // bigger og equal < or > and a :, compare and go to the key if true
+        
+        var valuesSplit = values.Substring(1, values.Length - 1).Split(",");
+        
+        // if (workflows.ContainsKey(workflow))
+        // {
+        //     return workflows[workflow] == "approved";
+        // }
+
+        return 0;
     }
 
     public long RunPart2(string[] input)
