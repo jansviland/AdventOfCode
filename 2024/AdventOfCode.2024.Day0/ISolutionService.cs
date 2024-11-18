65f6bad2e852dff2a602458f417b426767ea35d0
Jan Sviland <jan.sviland@systek.no>
2024-11-18 13:32:07 +0100

    sample project 2024, dotnet 9

---
+++ 2024/AdventOfCode.2024.Day0/ISolutionService.cs
@@ -0,0 +1,45 @@
+namespace AdventOfCode._2024.Day0;
+
+public interface ISolutionService
+{
+    public long RunPart1(string[] input);
+    public long RunPart2(string[] input);
+}
+
+public class SolutionService : ISolutionService
+{
+    private readonly ILogger<ISolutionService> _logger;
+
+    public SolutionService(ILogger<SolutionService> logger)
+    {
+        _logger = logger;
+    }
+
+    public long RunPart1(string[] input)
+    {
+        _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", GetYear(), GetDay());
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+    }
+
+    public long RunPart2(string[] input)
+    {
+        _logger.LogInformation("Solving - {Year} - Day {Day} - Part 2", GetYear(), GetDay());
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+    }
+    
+    private int GetYear()
+    {
+        var ns = GetType().Namespace;
+        return int.Parse(ns?.Split('.').ElementAt(1).Replace("_", "")!);
+    }
+    
+    private int GetDay()
+    {
+        var ns = GetType().Namespace;
+        return int.Parse(ns?.Split('.').Last().Replace("Day", "")!);
+    }
+}
