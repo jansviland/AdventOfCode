b83b8f71dc6f1b7bc60fd11bfb71ee35659abc99
Jan Sviland <jan.sviland@systek.no>
2024-12-02 10:34:24 +0100

    day 2: solution

--- 2024/AdventOfCode.2024.Day02.Tests/Tests.cs
+++ 2024/AdventOfCode.2024.Day02.Tests/Tests.cs
@@ -38,6 +38,6 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart2(_input);
 
         // assert
-        Assert.Equal(31, result);
+        Assert.Equal(4, result);
     }
 }
--- 2024/AdventOfCode.2024.Day02/ISolutionService.cs
+++ 2024/AdventOfCode.2024.Day02/ISolutionService.cs
@@ -21,10 +21,66 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        // get left column and right column, subtract each number in the right column from the corresponding number in the left column, sum the results
-        return Column(input, 0)
-            .Zip(Column(input, 1), (l, r) => Math.Abs(l - r))
-            .Sum();
+        var safeLines = 0;
+
+        foreach (var line in input)
+        {
+
+            int[] values = line.Split(" ").Select(int.Parse).ToArray();
+
+            if (IsSafe(values))
+            {
+                safeLines++;
+            }
+        }
+
+        return safeLines;
+    }
+
+    private bool IsSafe(int[] values)
+    {
+        _logger.LogInformation("Checking line: {Line}", string.Join(" ", values));
+        
+        // check if the values is increasing or decreasing
+        bool increasing = true;
+        int diff = values[0] - values[1];
+        if (diff < 0)
+        {
+            increasing = false;
+        }
+
+        for (var i = 0; i < values.Length - 1; i++)
+        {
+            var current = values[i];
+            var next = values[i + 1];
+            var diffToAdjacent = current - next;
+
+            if (diffToAdjacent < 0 && increasing)
+            {
+                _logger.LogInformation("Line was increasing and then decreased");
+                return false;
+            }
+            else if (diffToAdjacent > 0 && !increasing)
+            {
+                _logger.LogInformation("Line was decreasing and then increased");
+                return false;
+            }
+
+            var absDiff = Math.Abs(diffToAdjacent);
+            if (absDiff > 3)
+            {
+                _logger.LogInformation("Line had a difference greater than 3");
+                return false;
+            }
+
+            if (absDiff < 1)
+            {
+                _logger.LogInformation("Line had a difference less than 1");
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public long RunPart2(string[] input)
@@ -32,21 +88,38 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 2", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        // get right column and count the number of times each number appears, store in dictionary with number as key, count as value
-        var numberCount = Column(input, 1)
-            .CountBy(x => x)
-            .ToDictionary();
+        // for each line, check variations of the line where one value is removed
+        // example: 1 2 3 4 5
+        // if that does not work, attempt 2 3 4 5, then 1 3 4 5, then 1 2 4 5, then 1 2 3 5, then 1 2 3 4
 
-        // get left column, multiply each number by the count of the number in the right column, sum the results
-        // if the number is not in the dictionary, the count is 0 and we multiply by 0 so it doesn't affect the sum
-        return Column(input, 0)
-            .Select(num => numberCount.GetValueOrDefault(num) * num)
-            .Sum();
+        var safeLines = 0;
+        
+        foreach (var line in input)
+        {
+            var values = line.Split(" ").Select(int.Parse).ToArray();
+            
+            if (IsSafe(values))
+            {
+                safeLines++;
+                continue;
+            }
+            
+            for (var i = 0; i < values.Length; i++)
+            {
+                var newValues = values.ToList();
+                newValues.RemoveAt(i);
+                
+                if (IsSafe(newValues.ToArray()))
+                {
+                    safeLines++;
+                    break;
+                }
+            }
+            
+        }
+        
+        return safeLines;
     }
 
-    private static IEnumerable<int> Column(string[] input, int column) =>
-        from line in input
-        let nums = line.Split("   ").Select(int.Parse).ToArray()
-        orderby nums[column]
-        select nums[column];
+
 }
