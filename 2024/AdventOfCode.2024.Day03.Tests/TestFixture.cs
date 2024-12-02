8a11db467b25cd152c82d9dd286441d4852448ba
Jan Sviland <jan.sviland@systek.no>
2024-12-02 16:11:58 +0100

    prepare day 3

---
+++ 2024/AdventOfCode.2024.Day03.Tests/TestFixture.cs
@@ -0,0 +1,20 @@
+﻿using AdventOfCode._2024.Day03;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AdventOfCodeAdventOfCode._2024.Day03.Tests;
+
+public class TestFixture : TestBedFixture
+{
+    protected override void AddServices(IServiceCollection services, IConfiguration? configuration)
+        => services
+            .AddTransient<ISolutionService, SolutionService>();
+
+    protected override ValueTask DisposeAsyncCore()
+        => new();
+
+    protected override IEnumerable<TestAppSettings> GetTestAppSettings()
+    {
+        yield return new TestAppSettings { Filename = "appsettings.json", IsOptional = false };
+    }
+}
