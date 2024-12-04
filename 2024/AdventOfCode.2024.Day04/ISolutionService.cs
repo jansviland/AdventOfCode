6f184f36898adf6de3a5215fe391dd2e8b819674
Jan Sviland <jan.sviland@systek.no>
2024-12-04 15:16:02 +0100

    day 4: part 2

--- 2024/AdventOfCode.2024.Day04/ISolutionService.cs
+++ 2024/AdventOfCode.2024.Day04/ISolutionService.cs
@@ -20,18 +20,16 @@ public class SolutionService : ISolutionService
     {
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
-        
-        var regex = new Regex(@"XMAS|SAMX");
-        var regexXmas = new Regex(@"XMAS");
-        var regexSamx = new Regex(@"SAMX");
+
+        var regex = new Regex(@"(?=XMAS|SAMX)"); // we can not use (@"XMAS|SAMX"); as this will count XMASAMX as 1 match, it should be two
 
         var result = 0;
-        
+
         var allLines = new List<string>();
-        
+
         // horizontal lines
         allLines.AddRange(input);
-        
+
         // vertical lines
         var columns = new List<string>();
         for (var y = 0; y < input.Length; y++)
@@ -44,14 +42,14 @@ public class SolutionService : ISolutionService
                 {
                     columns.Add("");
                 }
-                
+
                 // for each character along the x axis, add it to the column y axis
                 // colums == lines, reversing the x and y axis
                 columns[x] += line[x];
             }
         }
         allLines.AddRange(columns);
-        
+
         // diagonal lines
         var diagonals = new List<string>();
         for (var y = 0; y < input.Length; y++)
@@ -64,21 +62,21 @@ public class SolutionService : ISolutionService
                 {
                     diagonals.Add("");
                 }
-                
+
                 // for each character along the x axis, add it to the diagonal
                 // diagonals == lines, reversing the x and y axis
                 diagonals[x + y] += line[x];
             }
         }
         allLines.AddRange(diagonals);
-        
+
         // diagonal lines reverse
         var diagonalsReverse = new List<string>();
         for (var y = 0; y < input.Length; y++)
         {
             // reverse the line
             var line = new string(input[y].Reverse().ToArray());
-            
+
             for (var x = 0; x < line.Length; x++)
             {
                 // if the diagonal does not exist, create it
@@ -86,26 +84,25 @@ public class SolutionService : ISolutionService
                 {
                     diagonalsReverse.Add("");
                 }
-                
+
                 // for each character along the x axis, add it to the diagonal
                 // diagonals == lines, reversing the x and y axis
                 diagonalsReverse[x + y] += line[x];
             }
         }
         allLines.AddRange(diagonalsReverse);
-        
+
         // count matches
         for (var i = 0; i < allLines.Count; i++)
         {
             var count = regex.Matches(allLines[i]).Count;
-            var countXmas = regexXmas.Matches(allLines[i]).Count;
-            var countSamx = regexSamx.Matches(allLines[i]).Count;
-            
+
             _logger.LogInformation("Found {Count} accurences of XMAS or SAMX in line {Line}", count, allLines[i]);
-            
-            result += countXmas + countSamx;
+
+            // result += countXmas + countSamx;
+            result += count;
         }
-        
+
         return result;
     }
 
@@ -114,6 +111,48 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 2", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        throw new NotImplementedException();
+        var count = 0;
+        for (var y = 0; y < input.Length; y++)
+        {
+            var line = input[y];
+            for (var x = 0; x < line.Length; x++)
+            {
+                var character = line[x];
+                if (character == 'A')
+                {
+                    var diagonalA = false;
+                    var diagonalB = false;
+
+                    // check top left is M and bottom right is S
+                    if (y > 0 && x > 0 && input[y - 1][x - 1] == 'M' && y < input.Length - 1 && x < line.Length - 1 && input[y + 1][x + 1] == 'S')
+                    {
+                        diagonalA = true;
+                    }
+                    // check if top left is S and bottom right is M
+                    else if (y > 0 && x > 0 && input[y - 1][x - 1] == 'S' && y < input.Length - 1 && x < line.Length - 1 && input[y + 1][x + 1] == 'M')
+                    {
+                        diagonalA = true;
+                    }
+
+                    // check top right is M and bottom left is S
+                    if (y > 0 && x < line.Length - 1 && input[y - 1][x + 1] == 'M' && y < input.Length - 1 && x > 0 && input[y + 1][x - 1] == 'S')
+                    {
+                        diagonalB = true;
+                    }
+                    // check if top right is S and bottom left is M
+                    else if (y > 0 && x < line.Length - 1 && input[y - 1][x + 1] == 'S' && y < input.Length - 1 && x > 0 && input[y + 1][x - 1] == 'M')
+                    {
+                        diagonalB = true;
+                    }
+
+                    if (diagonalA && diagonalB)
+                    {
+                        count++;
+                        _logger.LogInformation("Found X-MAS at {X},{Y}", x, y);
+                    }
+                }
+            }
+        }
+        return count;
     }
 }
