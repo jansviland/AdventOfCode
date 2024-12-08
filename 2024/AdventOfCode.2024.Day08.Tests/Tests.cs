49bc7aa2ec35527c21b51099a1033873e62d2071
Jan Sviland <jan.sviland@systek.no>
2024-12-08 14:16:04 +0100

    day 8: prepare

---
+++ 2024/AdventOfCode.2024.Day08.Tests/Tests.cs
@@ -0,0 +1,43 @@
+using AdventOfCode._2024.Day08;
+
+namespace AdventOfCodeAdventOfCode._2024.Day08.Tests;
+
+public class Tests : TestBed<TestFixture>
+{
+    private readonly ISolutionService _solutionService;
+    private readonly Helper _helper = new();
+
+    private readonly string[] _input;
+
+    public Tests(ITestOutputHelper testOutputHelper, TestFixture fixture) : base(testOutputHelper, fixture)
+    {
+        _solutionService = _fixture.GetService<ISolutionService>(_testOutputHelper)!;
+        _input = File.ReadAllLines("Assets/test-input.txt");
+    }
+
+    [Fact]
+    public void Part1Test()
+    {
+        // arrange
+        _testOutputHelper.WriteLine($"Running unit test - {_helper.GetYear()} - Day {_helper.GetDay()} - Part 1");
+
+        // act
+        var result = _solutionService.RunPart1(_input);
+
+        // assert
+        Assert.Equal(11, result);
+    }
+
+    [Fact]
+    public void Part2Test()
+    {
+        // arrange
+        _testOutputHelper.WriteLine($"Running unit test - {_helper.GetYear()} - Day {_helper.GetDay()} - Part 2");
+
+        // act
+        var result = _solutionService.RunPart2(_input);
+
+        // assert
+        Assert.Equal(31, result);
+    }
+}
---
+++ 2024/AdventOfCode.2024.Day08/ISolutionService.cs
@@ -0,0 +1,34 @@
+namespace AdventOfCode._2024.Day08;
+
+public interface ISolutionService
+{
+    public long RunPart1(string[] input);
+    public long RunPart2(string[] input);
+}
+
+public class SolutionService : ISolutionService
+{
+    private readonly ILogger<ISolutionService> _logger;
+    private readonly Helper _helper = new();
+
+    public SolutionService(ILogger<SolutionService> logger)
+    {
+        _logger = logger;
+    }
+
+    public long RunPart1(string[] input)
+    {
+        _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+    }
+
+    public long RunPart2(string[] input)
+    {
+        _logger.LogInformation("Solving - {Year} - Day {Day} - Part 2", _helper.GetYear(), _helper.GetDay());
+        _logger.LogInformation("Input contains {Input} values", input.Length);
+
+        throw new NotImplementedException();
+    }
+}
