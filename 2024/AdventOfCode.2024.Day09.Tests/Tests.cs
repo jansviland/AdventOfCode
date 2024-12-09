9676f8ba9b21240c328c39b22dd9555e1faded2e
Jan Sviland <jan.sviland@systek.no>
2024-12-09 10:10:35 +0100

    day 9: WIP

--- 2024/AdventOfCode.2024.Day09.Tests/Tests.cs
+++ 2024/AdventOfCode.2024.Day09.Tests/Tests.cs
@@ -25,7 +25,16 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart1(_input);
 
         // assert
-        Assert.Equal(11, result);
+        Assert.Equal(1928, result);
+    }
+
+    [Theory]
+    [InlineData("12345", "[0]..[1][1][1]....[2][2][2][2][2]")]
+    // [InlineData("2333133121414131402", "[0][0]...111...2...333.44.5555.6666.777.888899")]
+    public void ParseLineTest(string line, string expected)
+    {
+        var result = _solutionService.ParseLine(line);
+        Assert.Equal(expected, result);
     }
 
     [Fact]
--- 2024/AdventOfCode.2024.Day09/ISolutionService.cs
+++ 2024/AdventOfCode.2024.Day09/ISolutionService.cs
@@ -2,8 +2,9 @@ namespace AdventOfCode._2024.Day09;
 
 public interface ISolutionService
 {
-    public long RunPart1(string[] input);
-    public long RunPart2(string[] input);
+    long RunPart1(string[] input);
+    long RunPart2(string[] input);
+    string ParseLine(string line);
 }
 
 public class SolutionService : ISolutionService
@@ -20,8 +21,38 @@ public class SolutionService : ISolutionService
     {
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
+        
+        var parsed = ParseLine(input[0]);
+        _logger.LogInformation("Parsed: {Parsed}", parsed);
 
         throw new NotImplementedException();
+    }
+
+    public string ParseLine(string line)
+    {
+        var isFile = true;
+        var sb = new StringBuilder();
+        var currentFileNumber = 0;
+
+        for (var i = 0; i < line.Length; i++)
+        {
+            if (isFile)
+            {
+                var amount = int.Parse(line[i].ToString());
+                string currentFileNumberString = '[' + currentFileNumber.ToString() + ']';
+                sb.Append(string.Concat(Enumerable.Repeat(currentFileNumberString, amount)));
+                currentFileNumber++;
+            }
+            else
+            {
+                var amount = int.Parse(line[i].ToString());
+                sb.Append(string.Concat(Enumerable.Repeat('.', amount)));
+            }
+            
+            isFile = !isFile;
+        }
+
+        return sb.ToString();
     }
 
     public long RunPart2(string[] input)
