4e9b5a80cdf2a961fa4c5a6c5d7c6ab300c6ff09
Jan Sviland <jan.sviland@systek.no>
2024-12-09 16:06:14 +0100

    day 9: part 1

--- 2024/AdventOfCode.2024.Day09.Tests/Tests.cs
+++ 2024/AdventOfCode.2024.Day09.Tests/Tests.cs
@@ -25,15 +25,31 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart1(_input);
 
         // assert
-        Assert.Equal(1928, result);
+        Assert.Equal(1928UL, result);
     }
 
     [Theory]
     [InlineData("12345", "[0]..[1][1][1]....[2][2][2][2][2]")]
-    // [InlineData("2333133121414131402", "[0][0]...111...2...333.44.5555.6666.777.888899")]
+    [InlineData("2333133121414131402", "[0][0]...[1][1][1]...[2]...[3][3][3].[4][4].[5][5][5][5].[6][6][6][6].[7][7][7].[8][8][8][8][9][9]")]
     public void ParseLineTest(string line, string expected)
     {
         var result = _solutionService.ParseLine(line);
+        Assert.Equal(expected, result);
+    }
+    
+    [Theory]
+    [InlineData("[0]..[1][1][1]....[2][2][2][2][2]", "[0][2][2][1][1][1][2][2][2]......")]
+    public void ReOrderTest(string line, string expected)
+    {
+        var result = _solutionService.ReOrder(line);
+        Assert.Equal(expected, result);
+    }
+    
+    [Theory]
+    [InlineData("[0][0][9][9][8][1][1][1][8][8][8][2][7][7][7][3][3][3][6][4][4][6][5][5][5][5][6][6]............", 1928)]
+    public void CalcChecksumTest(string line, ulong expected)
+    {
+        var result = _solutionService.CalcChecksum(line);
         Assert.Equal(expected, result);
     }
 
--- 2024/AdventOfCode.2024.Day09/ISolutionService.cs
+++ 2024/AdventOfCode.2024.Day09/ISolutionService.cs
@@ -2,9 +2,12 @@ namespace AdventOfCode._2024.Day09;
 
 public interface ISolutionService
 {
-    long RunPart1(string[] input);
+    ulong RunPart1(string[] input);
     long RunPart2(string[] input);
+    
     string ParseLine(string line);
+    string ReOrder(string line);
+    ulong CalcChecksum(string line);
 }
 
 public class SolutionService : ISolutionService
@@ -17,15 +20,75 @@ public class SolutionService : ISolutionService
         _logger = logger;
     }
 
-    public long RunPart1(string[] input)
+    public ulong RunPart1(string[] input)
     {
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
-        
+
         var parsed = ParseLine(input[0]);
         _logger.LogInformation("Parsed: {Parsed}", parsed);
 
-        throw new NotImplementedException();
+        var reordered = ReOrder(parsed);
+        _logger.LogInformation("Reordered: {Reordered}", reordered);
+
+        var checksum = CalcChecksum(reordered);
+        _logger.LogInformation("Checksum: {Checksum}", checksum);
+        
+        return checksum;
+    }
+
+    public ulong CalcChecksum(string line)
+    {
+        var integers = line.Replace(".", string.Empty).Split(['[', ']'], StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToList();
+        
+        ulong count = 0;
+        for (var i = 0; i < integers.Count; i++)
+        {
+            count += (ulong) (integers[i] * i);
+        }
+        
+        return count;
+    }
+
+    public string ReOrder(string line)
+    {
+        int i = 0;
+        int removedCount = 0;
+        
+        var sb = new StringBuilder();
+        var currentLine = line;
+
+        while (currentLine.Length > 0 && i < currentLine.Length)
+        {
+            var lastOpening = currentLine.LastIndexOf('[');
+            
+            // take first empty . from the front
+            // take first number from the back
+            if (line[i] != '.' || i > lastOpening)
+            {
+                sb.Append(currentLine[i]);
+                i++;
+            }
+            else
+            {
+                // find first number [123] from the back
+                var lastClosing = currentLine.LastIndexOf(']');
+                var lastNumber = currentLine.Substring(lastOpening + 1, lastClosing - lastOpening - 1);
+
+                sb.Append('[' + lastNumber + ']');
+                
+                currentLine = currentLine.Substring(0, lastOpening);
+                removedCount++;
+                
+                i++;
+            }
+        }
+        
+        sb.Append(string.Concat(Enumerable.Repeat('.', removedCount)));
+
+        return sb.ToString();
     }
 
     public string ParseLine(string line)
@@ -48,7 +111,7 @@ public class SolutionService : ISolutionService
                 var amount = int.Parse(line[i].ToString());
                 sb.Append(string.Concat(Enumerable.Repeat('.', amount)));
             }
-            
+
             isFile = !isFile;
         }
 
