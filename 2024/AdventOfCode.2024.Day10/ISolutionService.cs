be05fdb6f0e940b42dd204e869ac21e8ee7c75b5
Jan Sviland <jan.sviland@systek.no>
2024-12-10 15:51:49 +0100

    refactor

--- 2024/AdventOfCode.2024.Day10/ISolutionService.cs
+++ 2024/AdventOfCode.2024.Day10/ISolutionService.cs
@@ -11,6 +11,15 @@ public interface ISolutionService
     public long RunPart2(string[] input);
 }
 
+public static class CharExtentions
+{
+    public static int ConvertToInt(this char c)
+    {
+        // https://stackoverflow.com/questions/239103/convert-char-to-int-in-c-sharp
+        return c - '0';
+    }
+}
+
 public class SolutionService : ISolutionService
 {
     private readonly ILogger<ISolutionService> _logger;
@@ -26,21 +35,27 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
         
+        // create a dictionary of all coordinates, with char as key and list of coordinates as values
         ILookup<char, Coor<int>> map = input
             .SelectMany((line, y) => line.Select((c, x) => (c, new Coor<int>(y, x))))
             .ToLookup(t => t.c, t => t.Item2);
+
+        // create a dictionary of all coordinates where the key is a number, char converted to 0-9 (not the ASCII value)
+        ILookup<int, Coor<int>> map2 = input
+            .SelectMany((line, y) => line.Select((c, x) => (c, new Coor<int>(y, x))))
+            .ToLookup(t => t.c.ConvertToInt(), t => t.Item2);
         
-        ICollection<Coor<int>> map2 = input
+        ICollection<Coor<int>> map3 = input
             .SelectMany((line, y) => line.Select((c, x) => (c, new Coor<int>(y, x))))
             .Select(t => t.Item2)
             .ToList();
 
-        map2.Visualize(null, null);
+        map3.Visualize(null, null);
         
 
         throw new NotImplementedException();
     }
-
+    
     public long RunPart2(string[] input)
     {
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 2", _helper.GetYear(), _helper.GetDay());
