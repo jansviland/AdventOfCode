e8db5bce17409babaeeca7a72de8978a9e33b8f0
Jan Sviland <jan.sviland@systek.no>
2024-12-18 16:36:08 +0100

    day 8: WIP

--- 2024/AdventOfCode.2024.Day08/ISolutionService.cs
+++ 2024/AdventOfCode.2024.Day08/ISolutionService.cs
@@ -28,13 +28,38 @@ public class SolutionService : ISolutionService
         return map;
     }
 
+    void Print(Dictionary<Complex, char> map)
+    {
+        var sb = new StringBuilder();
+        sb.AppendLine();
+
+        var maxX = map.Keys.Max(c => c.Real);
+        var maxY = map.Keys.Max(c => c.Imaginary);
+
+        for (var y = 0; y <= maxY; y++)
+        {
+            for (var x = 0; x <= maxX; x++)
+            {
+                var key = new Complex(x, y);
+                sb.Append(map.TryGetValue(key, out char value) ? value : '.');
+                sb.Append(' ');
+            }
+            sb.AppendLine();
+        }
+
+        _logger.LogInformation(sb.ToString());
+    }
+
     public long RunPart1(string[] input)
     {
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
         var map = Parse(input);
+        Print(map);
         
+        
+
         throw new NotImplementedException();
     }
 
