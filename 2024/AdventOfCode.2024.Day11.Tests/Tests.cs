77a91bd0e14e0edfee9a556646e81e6763430910
Jan Sviland <jan.sviland@systek.no>
2024-12-29 15:55:37 +0100

    day 11: part 2

--- 2024/AdventOfCode.2024.Day11.Tests/Tests.cs
+++ 2024/AdventOfCode.2024.Day11.Tests/Tests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using AdventOfCode._2024.Day11;
 
 namespace AdventOfCodeAdventOfCode._2024.Day11.Tests;
@@ -15,24 +16,6 @@ public class Tests : TestBed<TestFixture>
         _input = File.ReadAllLines("Assets/test-input.txt");
     }
     
-    
-    // The first stone, 0, becomes a stone marked 1.
-    // The second stone, 1, is multiplied by 2024 to become 2024.
-    // The third stone, 10, is split into a stone marked 1 followed by a stone marked 0.
-    // The fourth stone, 99, is split into two stones marked 9.
-    // The fifth stone, 999, is replaced by a stone marked 2021976.
-    [Theory]
-    [InlineData(0, 1)]
-    [InlineData(1, 2024)]
-    [InlineData(10, 11)]
-    [InlineData(99, 18)]
-    [InlineData(999, 2021976)]
-    public void Blink_Test(long n, long expected)
-    {
-        Assert.Equal(expected, _solutionService.Blink(n, 1));
-    }
-    
-    
     /// <summary>
     /// The first stone, 0, becomes a stone marked 1.
     /// The second stone, 1, is multiplied by 2024 to become 2024.
@@ -48,81 +31,12 @@ public class Tests : TestBed<TestFixture>
     [InlineData(10, 2)]
     [InlineData(99, 2)]
     [InlineData(999, 1)]
-    public void Blink_Recursive_Test(long n, long expected)
+    public void Blink_Test(long n, long expected)
     {
-        Assert.Equal(expected, _solutionService.Blink(n, 1));
-    }
+        // we use a concurrent dictionary type that is thread safe
+        var cache = new ConcurrentDictionary<(long, int), long>();
 
-    // After 1 blink:
-    // 253000 1 7
-    //
-    // After 2 blinks:
-    // 253 0 2024 14168
-    //
-    // After 3 blinks:
-    // 512072 1 20 24 28676032
-    //
-    // After 4 blinks:
-    // 512 72 2024 2 0 2 4 2867 6032
-    //
-    // After 5 blinks:
-    // 1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32
-    //
-    // After 6 blinks:
-    // 2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2
-    
-    [Fact]
-    public void Blink_1Blink_Test()
-    {
-        var stones = new List<uint> { 125, 17 };
-        var expected = new List<uint> { 253000, 1, 7 };
-
-        Assert.Equal(expected, _solutionService.Blink(stones, 1));
-    }
-    
-    [Fact]
-    public void Blink_2Blinks_Test()
-    {
-        var stones = new List<uint> { 125, 17 };
-        var expected = new List<uint> { 253, 0, 2024, 14168 };
-
-        Assert.Equal(expected, _solutionService.Blink(stones, 2));
-    }
-    
-    [Fact]
-    public void Blink_3Blinks_Test()
-    {
-        var stones = new List<uint> { 125, 17 };
-        var expected = new List<uint> { 512072, 1, 20, 24, 28676032 };
-
-        Assert.Equal(expected, _solutionService.Blink(stones, 3));
-    }
-
-    [Fact]
-    public void Blink_4Blinks_Test()
-    {
-        var stones = new List<uint> { 125, 17 };
-        var expected = new List<uint> { 512, 72, 2024, 2, 0, 2, 4, 2867, 6032 };
-
-        Assert.Equal(expected, _solutionService.Blink(stones, 4));
-    }
-    
-    [Fact]
-    public void Blink_5Blinks_Test()
-    {
-        var stones = new List<uint> { 125, 17 };
-        var expected = new List<uint> { 1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32 };
-
-        Assert.Equal(expected, _solutionService.Blink(stones, 5));
-    }
-
-    [Fact]
-    public void Blink_6Blinks_Test()
-    {
-        var stones = new List<uint> { 125, 17 };
-        var expected = new List<uint> { 2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40, 48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2 };
-
-        Assert.Equal(expected, _solutionService.Blink(stones, 6));
+        Assert.Equal(expected, _solutionService.Blink(n, 1, cache));
     }
 
     [Fact]
@@ -138,16 +52,16 @@ public class Tests : TestBed<TestFixture>
         Assert.Equal(55312, result);
     }
 
-    [Fact]
-    public void Part2Test()
-    {
-        // arrange
-        _testOutputHelper.WriteLine($"Running unit test - {_helper.GetYear()} - Day {_helper.GetDay()} - Part 2");
-
-        // act
-        var result = _solutionService.RunPart2(_input);
-
-        // assert
-        Assert.Equal(55312, result);
-    }
+    // [Fact]
+    // public void Part2Test()
+    // {
+    //     // arrange
+    //     _testOutputHelper.WriteLine($"Running unit test - {_helper.GetYear()} - Day {_helper.GetDay()} - Part 2");
+    //
+    //     // act
+    //     var result = _solutionService.RunPart2(_input);
+    //
+    //     // assert
+    //     Assert.Equal(55312, result);
+    // }
 }
--- 2024/AdventOfCode.2024.Day11/ISolutionService.cs
+++ 2024/AdventOfCode.2024.Day11/ISolutionService.cs
@@ -6,8 +6,7 @@ public interface ISolutionService
 {
     public long RunPart1(string[] input);
     public long RunPart2(string[] input);
-    public List<uint> Blink(List<uint> stones, int times);
-    public long Blink(long n, int blinks);
+    public long Blink(long n, int blinks, ConcurrentDictionary<(long n, int blinks), long> cache);
 }
 
 public class SolutionService : ISolutionService
@@ -20,27 +19,8 @@ public class SolutionService : ISolutionService
         _logger = logger;
     }
 
-    // 1. We can solve the task recusivly
-    public long Blink(long n, int blinks)
-    {
-        switch ((n, blinks))
-        {
-            case (n: _, blinks: 0): return 1;
-            case (n: 0, blinks: _): return Blink(1, blinks - 1);
-            case (n: var stone, _) when stone.ToString().Length % 2 == 0:
-
-                var str = stone.ToString();
-                var part1 = str.Substring(0, str.Length / 2);
-                var part2 = str.Substring(str.Length / 2);
-
-                return Blink(long.Parse(part1), blinks - 1) + Blink(long.Parse(part2), blinks - 1);
-
-            case (n: var stone, _): return Blink(stone * 2024, blinks - 1);
-        }
-    }
-
-    // 2. We can store the results for "input string + nr of blinks", to a dictinary, so we don't repeat calculations (cache)
-    long Blink(long n, int blinks, ConcurrentDictionary<(long n, int blinks), long> cache)
+    // add cache to improve preformance
+    public long Blink(long n, int blinks, ConcurrentDictionary<(long n, int blinks), long> cache)
     {
         // store result of each calculation, and check if it has been calculated already to avoid repeating the same calculation multiple times. 
         // example: 
@@ -50,22 +30,13 @@ public class SolutionService : ISolutionService
         // 
         // here we can se that we have stone "2" and blink "6", 4 times, we also have "40" twice and "48" twice. By storing these results in a lookup
         // dictionary we only do these calculations once
-            
-        // _logger.LogInformation("Test: {n} - {blinks}", n, blinks);
-
-        if (cache.TryGetValue((n, blinks), out long result))
-        {
-            _logger.LogInformation("Cache hit: {n} - {blinks} - {result}", n, blinks, result);
-            // return result;
-        }
-
         return cache.GetOrAdd((n, blinks), key =>
         {
             switch (key)
             {
-                case (n: _, blinks: 0): return cache[(n, blinks)] = 1;
+                case (n: _, blinks: 0): return 1;
 
-                case (n: 0, blinks: _): return cache[(n, blinks)] = Blink(1, blinks - 1);
+                case (n: 0, blinks: _): return Blink(1, blinks - 1, cache);
 
                 case (n: var stone, _) when stone.ToString().Length % 2 == 0:
 
@@ -73,11 +44,9 @@ public class SolutionService : ISolutionService
                     var part1 = str.Substring(0, str.Length / 2);
                     var part2 = str.Substring(str.Length / 2);
 
-                    cache[(n, blinks)] = Blink(long.Parse(part1), blinks - 1) + Blink(long.Parse(part2), blinks - 1);
+                    return Blink(long.Parse(part1), blinks - 1, cache) + Blink(long.Parse(part2), blinks - 1, cache);
 
-                    return cache[(n, blinks)];
-
-                case (n: var stone, _): return cache[(n, blinks)] = Blink(stone * 2024, blinks - 1);
+                case (n: var stone, _): return Blink(stone * 2024, blinks - 1, cache);
             }
         });
     }
@@ -87,7 +56,10 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        return input[0].Split(" ").Select(long.Parse).Select(x => Blink(x, 25)).Sum();
+        // we use a concurrent dictionary type that is thread safe
+        var cache = new ConcurrentDictionary<(long, int), long>();
+
+        return input[0].Split(" ").Select(long.Parse).Select(x => Blink(x, 25, cache)).Sum();
     }
 
     public long RunPart2(string[] input)
@@ -98,57 +70,6 @@ public class SolutionService : ISolutionService
         // we use a concurrent dictionary type that is thread safe
         var cache = new ConcurrentDictionary<(long, int), long>();
 
-        return input[0].Split(" ").Select(long.Parse).Select(x => Blink(x, 75, cache)).Sum();
-    }
-
-    public List<uint> ApplyRules(uint stone)
-    {
-        var result = new List<uint>();
-        if (stone == 0)
-        {
-            result.Add(1);
-        }
-        else if (stone.ToString().Length % 2 == 0)
-        {
-            var str = stone.ToString();
-            var right = str.Substring(str.Length / 2);
-            var left = str.Substring(0, str.Length / 2);
-
-            if (ulong.TryParse(left, out _))
-            {
-                result.Add(uint.Parse(left));
-            }
-
-            if (ulong.TryParse(right, out _))
-            {
-                result.Add(uint.Parse(right));
-            }
-        }
-        else
-        {
-            result.Add(stone * 2024);
-        }
-
-        // _logger.LogInformation("{Stone} -> {Result}", stone, string.Join(", ", result));
-
-        return result;
-    }
-
-    public List<uint> Blink(List<uint> stones, int times)
-    {
-        var current = stones;
-        for (var i = 0; i < times; i++)
-        {
-            var updated = new List<uint>();
-            foreach (uint stone in current)
-            {
-                updated.AddRange(ApplyRules(stone));
-            }
-
-            // _logger.LogInformation("After {Current} blink: {Stones}", i, string.Join(", ", updated));
-            current = updated;
-        }
-
-        return current;
+        return input[0].Split(" ").Select(long.Parse).Sum(n => Blink(n, 75, cache));
     }
 }
