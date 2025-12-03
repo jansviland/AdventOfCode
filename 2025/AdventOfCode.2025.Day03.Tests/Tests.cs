5b02538373eb942d8f9a9bb765fddb850b1e7ad2
Jan Sviland <jan.sviland@systek.no>
2025-12-03 11:19:27 +0100

    part 2

--- 2025/AdventOfCode.2025.Day03.Tests/Tests.cs
+++ 2025/AdventOfCode.2025.Day03.Tests/Tests.cs
@@ -27,7 +27,15 @@ public class Tests : TestBed<TestFixture>
         // assert
         Assert.Equal(357, result);
     }
-    
+
+    [Theory]
+    [InlineData("819", 2, 89)]
+    [InlineData("1819", 3, 819)]
+    public void HigestValueComboTest(string str, int total, long result)
+    {
+        Assert.Equal(result, _solutionService.HighestValueCombo(str, total));
+    }
+
     [Fact]
     public void Part2Test()
     {
@@ -38,6 +46,6 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart2(_inputTest);
 
         // assert
-        Assert.Equal(4174379265, result);
+        Assert.Equal(3121910778619, result);
     }
 }
--- 2025/AdventOfCode.2025.Day03/ISolutionService.cs
+++ 2025/AdventOfCode.2025.Day03/ISolutionService.cs
@@ -4,6 +4,7 @@ public interface ISolutionService
 {
     public long RunPart1(string[] input);
     public long RunPart2(string[] input);
+    long HighestValueCombo(string str, int total);
 }
 
 public class SolutionService : ISolutionService
@@ -16,38 +17,72 @@ public class SolutionService : ISolutionService
         _logger = logger;
     }
 
-    IEnumerable<int> Parse(string[] input) =>
+    // IEnumerable<int> Parse(string[] input) =>
+    //     from line in input
+    //     let result = highestValuePair(line)
+    //     select result;
+
+    IEnumerable<long> Parse2(string[] input, int total) =>
         from line in input
-        let result = highestValuePair(line)
+        let result = HighestValueCombo(line, total)
         select result;
 
-    public int highestValuePair(string str)
+    // public int highestValuePair(string str)
+    // {
+    //     var left = 0;
+    //     var leftIndex = 0;
+    //     for (int i = 0; i < str.Length - 1; i++) // we must have at least one value to the right in the end, so it can not be the last element
+    //     {
+    //         var num = str[i] - '0'; // assuming value 0-9, this is the fastest way to get the int from char
+    //         if (num > left)
+    //         {
+    //             left = num;
+    //             leftIndex = i;
+    //         }
+    //     }
+    //
+    //     var right = 0;
+    //     for (int i = leftIndex + 1; i < str.Length; i++) // start from left index + 1, can not have the same bank twice
+    //     {
+    //         var num = str[i] - '0';
+    //         if (num > right)
+    //         {
+    //             right = num;
+    //         }
+    //     }
+    //
+    //     var result = left.ToString() + right.ToString();
+    //
+    //     return int.Parse(result);
+    // }
+
+    public long HighestValueCombo(string str, int total)
     {
-        var left = 0;
-        var leftIndex = 0;
-        for (int i = 0; i < str.Length - 1; i++) // we must have at least one value to the right in the end, so it can not be the last element
-        {
-            var num = str[i] - '0'; // assuming value 0-9, this is the fastest way to get the int from char
-            if (num > left)
-            {
-                left = num;
-                leftIndex = i;
-            } 
-        }
-
-        var right = 0;
-        for (int i = leftIndex + 1; i < str.Length; i++) // start from left index + 1, can not have the same bank twice
-        {
-            var num = str[i] - '0';
-            if (num > right)
-            {
-                right = num;
-            }
-        }
-
-        var result = left.ToString() + right.ToString();
+        var sb = new StringBuilder();
+        var currentBank = 0;
+        var nextIndex = 0;
         
-        return int.Parse(result);
+        while (currentBank < total)
+        {
+            var curr = -1;
+            var remaining = total - currentBank;
+            
+            for (int i = nextIndex; i <= str.Length - remaining; i++) // we must have at least one value to the right in the end, so it can not be the last element
+            {
+                var num = str[i] - '0'; // assuming value 0-9, this is the fastest way to get the int from char
+                if (num > curr)
+                {
+                    curr = num;
+                    nextIndex = i;
+                }
+            }
+
+            sb.Append(str[nextIndex]);
+            currentBank++;
+            nextIndex++;
+        }
+
+        return long.Parse(sb.ToString());
     }
 
     public long RunPart1(string[] input)
@@ -55,7 +90,8 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        return Parse(input).Sum();
+        // return Parse(input).Sum();
+        return Parse2(input, 2).Sum();
     }
 
     public long RunPart2(string[] input)
@@ -63,8 +99,7 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 2", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        // return Repeates(Parse1(input)).Sum();
-        throw new NotImplementedException();
+        return Parse2(input, 12).Sum();
     }
 
 }
