4b8265b8be380ede75632cd08af01e05f7470289
Jan Sviland <jan.sviland@systek.no>
2025-12-07 15:48:28 +0100

    part 1

--- 2025/AdventOfCode.2025.Day05.Tests/Tests.cs
+++ 2025/AdventOfCode.2025.Day05.Tests/Tests.cs
@@ -38,6 +38,6 @@ public class Tests : TestBed<TestFixture>
         var result = _solutionService.RunPart2(_inputTest);
 
         // assert
-        Assert.Equal(43, result);
+        Assert.Equal(14, result);
     }
 }
--- 2025/AdventOfCode.2025.Day05/ISolutionService.cs
+++ 2025/AdventOfCode.2025.Day05/ISolutionService.cs
@@ -6,6 +6,8 @@ public interface ISolutionService
     public long RunPart2(string[] input);
 }
 
+record Range(long start, long end);
+
 public class SolutionService : ISolutionService
 {
     private readonly ILogger<ISolutionService> _logger;
@@ -16,23 +18,79 @@ public class SolutionService : ISolutionService
         _logger = logger;
     }
 
-    // IEnumerable<int> Parse(string[] input) => 
-    //     from line in input
-    //     let split = line.Split('-')
-    //     let start = int.Parse(split[0])
-    //     let end = int.Parse(split[1])
-    //     let range = Enumerable.Range(start, end)
-    //     select range;
-    //         
+    IEnumerable<long> RangeLong(Range range)
+    {
+        for (long i = range.start; i <= range.end; i++)
+            yield return i;
+    }
 
+    IEnumerable<Range> ParseRanges(string[] input)
+    {
+        var ranges = new List<Range>();
+        for (var i = 0; i < input.Length; i++)
+        {
+            var line = input[i];
+            if (!line.Contains('-'))
+            {
+                break;
+            }
+
+            var split = line.Split('-');
+            var start = long.Parse(split[0]);
+            var end = long.Parse(split[1]);
+
+            ranges.Add(new Range(start, end));
+        }
+
+        return ranges;
+    }
+
+    IEnumerable<long> ParseIngredientId(string[] input)
+    {
+        var list = new List<long>();
+        for (var i = 0; i < input.Length; i++)
+        {
+            var line = input[i];
+            if (line.Contains('-'))
+            {
+                continue;
+            }
+
+            var isNum = long.TryParse(line, out long result);
+            if (isNum)
+            {
+                list.Add(result);
+            }
+
+        }
+
+        return list;
+    }
 
     public long RunPart1(string[] input)
     {
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 1", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
-        // var grid = Parse(input);
-        throw new NotImplementedException();
+        var ranges = ParseRanges(input);
+        var ingredients = ParseIngredientId(input);
+
+        return ingredients.Count(num => ranges.Any(r => num >= r.start && num <= r.end));
+
+        // var count = 0;
+        // foreach (long i in ingredients)
+        // {
+        //     foreach ((long start, long end) in ranges)
+        //     {
+        //         if (i >= start && i <= end)
+        //         {
+        //             count++;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // return count;
     }
 
     public long RunPart2(string[] input)
@@ -40,6 +98,16 @@ public class SolutionService : ISolutionService
         _logger.LogInformation("Solving - {Year} - Day {Day} - Part 2", _helper.GetYear(), _helper.GetDay());
         _logger.LogInformation("Input contains {Input} values", input.Length);
 
+        // var ranges = ParseRanges(input);
+        // var uniqueValues = ranges
+        //     .SelectMany(r => RangeLong(r.start, r.end))
+        //     .Distinct()
+        //     .Count();
+        //     // .OrderBy(x => x);
+        //
+        // return uniqueValues;
+        //
+        
         throw new NotImplementedException();
     }
 
